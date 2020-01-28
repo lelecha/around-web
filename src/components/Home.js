c97f50086d7812bc4bfd3a1f18c175242bce4cf2
Guoqi <maguoqi96@gmail.com>
2020-01-27 22:18:48 -0500

    Added tabs in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
 import '../styles/Home.css';
 
+const { TabPane } = Tabs;
+const operations = <Button>Create new post</Button>;
+
 export function Home() {
-    return (<div>Home</div>);
+    return (
+        <Tabs tabBarExtraContent={operations} className={'main-tabs'}>
+            <TabPane tab="Tab 1" key="1">
+                Content of tab 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+                Content of tab 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+                Content of tab 3
+            </TabPane>
+        </Tabs>);
 };
