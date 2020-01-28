038cf5f1e361b3763255ffbf64a0b8106cdea501
Guoqi <maguoqi96@gmail.com>
2020-01-27 22:51:21 -0500

    Getting Geolocation in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,67 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
+import { GEOLOCATION_OPTIONS } from '../constants';
 import '../styles/Home.css';
 
 const { TabPane } = Tabs;
-const operations = <Button>Create new post</Button>;
 
-export function Home() {
-    return (
-        <Tabs tabBarExtraContent={operations} className={'main-tabs'}>
-            <TabPane tab="Tab 1" key="1">
-                Content of tab 1
-            </TabPane>
-            <TabPane tab="Tab 2" key="2">
-                Content of tab 2
-            </TabPane>
-            <TabPane tab="Tab 3" key="3">
-                Content of tab 3
-            </TabPane>
-        </Tabs>);
+export class Home extends React.Component {
+    state = {
+        loadingGeolocation: false,
+        errorMessage: null,
+    }
+
+    getGeolocation() {
+        this.setState({
+            loadingGeolocation: true,
+            errormessage: null,
+        });
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onGeolocationSuccess,
+                this.onGeolocationFailure,
+                GEOLOCATION_OPTIONS,
+            );
+        } else {
+            this.setState({
+                loadingGeolocation: false,
+                errormessage: 'Your browser does not support geolocation.',
+            });
+        }
+    }
+
+    onGeolocationSuccess = (position) => {
+        this.setState({
+            loadingGeolocation: false,
+            errormessage: null,
+        })
+        console.log(position);
+    }
+
+    onGeolocationFailure = () => {
+        this.setState({
+            loadingGeolocation: false,
+            errormessage: 'Failed to load geolocation.',
+        })
+    }
+
+    componentDidMount() {
+        this.getGeolocation();
+    }
+    render() {
+        const operations = <Button>Create New Post</Button>;
+        return (
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Tab 1" key="1">
+                    Content of tab 1
+                </TabPane>
+                <TabPane tab="Tab 2" key="2">
+                    Content of tab 2
+                </TabPane>
+                <TabPane tab="Tab 3" key="3">
+                    Content of tab 3
+                </TabPane>
+            </Tabs>
+        );
+    }
 };
