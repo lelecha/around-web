82ce951ab060352784c6acf5b5221fa30d864903
Guoqi <maguoqi96@gmail.com>
2020-02-03 22:38:52 -0500

    Added InfoWindow

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -2,11 +2,23 @@ import React from 'react';
 import {
     withScriptjs,
     withGoogleMap,
+    InfoWindow,
     GoogleMap,
     Marker,
 } from "react-google-maps";
 
 class NormalAroundMap extends React.Component {
+
+    state = {
+        isOpen: false,
+    }
+
+    onToggleOpen = () => {
+        this.setState(({ isOpen }) => ({
+            isOpen: !isOpen,
+        }))
+    }
+
     render() {
         return (
             <GoogleMap
@@ -15,7 +27,18 @@ class NormalAroundMap extends React.Component {
             >
                 <Marker
                     position={{ lat: -34.397, lng: 150.644 }}
-                />
+                    onClick={this.onToggleOpen}
+                >
+                    {(
+                        this.state.isOpen ?
+                            (
+                                <InfoWindow onCloseClick={this.onToggleOpen}>
+                                    <div>InfoWindow</div>
+                                </InfoWindow>
+                            ) :
+                            null
+                    )}
+                </Marker>
             </GoogleMap>
         );
     }
